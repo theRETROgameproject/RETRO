70cafafd5a62223460d1a9de044318010936dac4
s1m0n-github <simon.waldek@gmail.com>
2023-07-02 21:16:29 +0200

    a lot of shit

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,23 @@
 extends Node
+var coins = 0
+var Level2 = preload("res://scenes/Level2.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func cointer():
+	coins += 1
+	var coinlabel = get_child(0).get_node("CanvasLayer/Label")
+	coinlabel.text = str("Coins: ", coins)
+
+func level2():
+	get_child(0).queue_free()
+	var next_level = Level2.instantiate()
+	add_child(next_level)
