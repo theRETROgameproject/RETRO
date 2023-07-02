16b870daa7a3d77834cca457298795263ffc39e2
WavyCat <maxkurtenbach@outlook.de>
2023-07-02 12:44:15 +0200

    Hallo SImon

--- scenes/Gegner.gd
+++ scenes/Gegner.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var startpos = position.x
 
 func _physics_process(delta):
 	
@@ -14,12 +14,21 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		
 	var playerXpos = get_parent().get_parent().get_node("PlayerLink").position.x
-	if position.x-playerXpos > 5:
+	var deltaXPlayer = position.x-playerXpos
+	var deltaXStart = position.x-startpos
+	if deltaXPlayer > 5 && deltaXPlayer < 150:
 		velocity.x = -50
-	elif position.x-playerXpos < -5: 
+	elif deltaXPlayer < -5 && deltaXPlayer > -150: 
 		velocity.x = 50
 	else:
-		velocity.x = 0
+		if deltaXStart > 20:
+			velocity.x = -20
+			if deltaXStart == 20:
+				velocity.x = 20
+		elif deltaXStart < -20:
+			velocity.x = 20
+			if deltaXStart == -20:
+				velocity.x = -20
 		
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
