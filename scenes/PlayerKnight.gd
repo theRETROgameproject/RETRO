d1030a4d24480024fbaf06a26f3bffb6eec46318
s1m0n-github <simon.waldek@gmail.com>
2023-07-09 23:35:33 +0200

    asjd

--- scenes/PlayerKnight.gd
+++ scenes/PlayerKnight.gd
@@ -6,7 +6,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	get_child(2).get_child(0).fade_in()
-	velocity.x = 27
+	velocity.x = 27#27
 
 func anime():
 	if dead:
@@ -32,6 +32,7 @@ func _physics_process(delta):
 func _on_gommehd_net_body_entered(body):
 	if body.name == "PlayerKnight":
 		velocity.x = 0
+		
 		get_child(2).get_child(0).blitz()
 		await get_tree().create_timer(1.0).timeout
 		dead = true
